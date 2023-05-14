94019cbceaa77f6c8167e30627b99a04bef704d8
DCaldwell13 <draykcaldwell0516@gmail.com>
2023-05-14 11:26:10 -0700

    vercel

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
+require("dotenv").config();
+const PORT = process.env.PORT;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(3007)
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+
+module.exports = app;
